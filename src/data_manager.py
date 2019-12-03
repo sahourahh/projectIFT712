35449e9020172492a652a026e9f9951ba39ee53e
Ch@ngeUP2019 <ikram20192019>
2019-12-03 18:45:30 -0500

    extract data and targets

--- src/data_manager.py
+++ src/data_manager.py
@@ -4,6 +4,14 @@
 # Sahar Tahir (19 145 088)       #
 ##################################
 
+from __future__ import division
+from PIL import Image
+ 	
+#from path import path
+import os
+import matplotlib.pyplot
+####################
+
 import pandas as pd 
 import numpy as np
 import random 
@@ -13,81 +21,100 @@ from sklearn.model_selection import StratifiedShuffleSplit
 
 import matplotlib.pyplot
 
+
 class DataManager:
 
     _train_file = "../ressources/leaf-classification/train.csv"
-    _images_repo = "../ressources/leaf-classification/images"
-    _nb_test_data = 0.2
+    _images_repo = "../ressources/leaf-classification/images/"    
     _r = random.randint(17, 27)
     _X_train = pd.DataFrame()
     _X_test = pd.DataFrame()
     _y_train = pd.DataFrame()
     _y_test = pd.DataFrame()
     _classes = []
+    _TrainTargets=[]
+    _TestTargets=[]
+
+
+
+    def __init__(self,nb_test_data = 0.2):
+        ###nb_test_data is the percentage of test data from the original file
+        self._nb_test_data = nb_test_data
              
     def _extractBasicData(self):
         """
         This function generates train and test data
-
         """
         train=pd.read_csv(self._train_file)        
-        s = LabelEncoder().fit(train.species)  #len=99 return a list of all species
-        self._classes = list(s.classes_)  # list of all classes,   position=code
-        classes_labels = s.transform(train.species)  #len=990 Encode labels with value between 0 and n_classes-1.
+        s = LabelEncoder().fit(train.species)  
+        self._classes = list(s.classes_)  
+        classes_labels = s.transform(train.species) 
         train = train.drop(['species', 'id'], axis=1) 
         sss = StratifiedShuffleSplit(n_splits=1,  test_size=self._nb_test_data, random_state=self._r)
-        for train_index, test_index in sss.split(train, classes_labels):   #n_splits is the nbre of iterations
-            self._X_train, self._X_test = train.values[train_index], train.values[test_index]  #train_index:indexes of train data
+        for train_index, test_index in sss.split(train, classes_labels):  
+            self._X_train, self._X_test = train.values[train_index], train.values[test_index]  
             self._y_train, self._y_test = classes_labels[train_index], classes_labels[test_index]
 
     def _extractTrainTargets(self):
         """
         This function generates Train targets
-        :return: One hot vector for train data
         """
         if len(self._classes)==0 or self._y_train.shape==(0,0):
             self._extractBasicData()
         T=np.zeros([len(self._y_train),len(self._classes)])
         for i in range(0,len(self._y_train)):
             T[i,self._y_train[i]]=1
-        return  T
+        self._TrainTargets= T
     
     def _extractTestTargets(self):
         """
         This function generates Test targets
-        :return: One hot vector for test data
         """
         if len(self._classes)==0 or self._y_test.shape==(0,0):
             self._extractBasicData()
         T=np.zeros([len(self._y_test),len(self._classes)])
         for i in range(0,len(self._y_test)):
             T[i,self._y_test[i]]=1
-        return  T
+        self._TestTargets = T
 
 ###########################################################################################################################################
     
-    def getBasicTrainTestData(self):          
+    def getBasicTrainData(self):          
         """
-        This function calls the private function _extractBasicData() to extract data
+        This function calls the private function _extractBasicData() to extract train data
         :return: _X_train: Train matrix
-                 _X_test : Test matrix
         """
-        self._extractBasicData() 
-        return self._X_train , self._X_test 
+        if len(self._X_train)==0 :
+            self._extractBasicData() 
+        return self._X_train
+        
+    def getBasicTestData(self):          
+        """
+        This function calls the private function _extractBasicData() to extract test data
+        :return: _X_test : Test matrix
+        """
+        if len(self._X_test)==0 :
+            self._extractBasicData() 
+        return  self._X_test 
+
 
     def getTrainTargets(self):        
         """
-        This function calls the private function _extractTrainTargets() to extract train Targets
-        :return: The result of _extractTrainTargets() which is a one hot vector
+        This function calls the private function _extractTrainTargets() to extract train Targets if they aren't already extracted
+        :return: The result of _extractTrainTargets() which is a matrix of one hot vector
         """
-        return self._extractTrainTargets()
+        if len(self._TrainTargets)==0 :
+            self._extractTrainTargets()  
+        return self._TrainTargets
 
     def getTestTargets(self):
         """
-        This function calls the private function _extractTestTargets() to extract test Targets
-        :return: The result of _extractTestTargets() which is a one hot vector
-        """        
-        return self._extractTestTargets()
+        This function calls the private function _extractTestTargets() to extract test Targets if they aren't already extracted
+        :return: The result of _extractTestTargets() which is a matrix of one hot vector
+        """  
+        if len(self._TestTargets)==0 :
+            self._extractTestTargets()      
+        return self._TestTargets
 
     def getListOfClasses(self):
         """
@@ -97,7 +124,6 @@ class DataManager:
         if len(self._classes)==0 :
             self._extractBasicData()   
         return self._classes
-    
 
 
     
