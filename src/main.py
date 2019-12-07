ee267013b7b2b0fadd7533f4a46187b3abe9fc75
sahar <tahir.sahar97@gmail.com>
2019-12-07 14:38:00 -0500

    class classifier to define train,predict and error methods

--- src/main.py
+++ src/main.py
@@ -1,14 +1,6 @@
 
 from classifiers.classifier import Classifier
 def main ():
-    X = [[1, 3, 4], [6, 3, 2]]
-
-    t=[[1,0],[0,1]]
-
-    me = Classifier()
-
-    print(me.train())
-    print(me.prediction(X))
 
 
 
