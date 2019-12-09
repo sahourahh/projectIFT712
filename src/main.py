edd834389719d184cbe423f5f041c6275eaabe82
sahar <tahir.sahar97@gmail.com>
2019-12-09 17:20:29 -0500

    class classifier to define train,predict and error methods

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
-import numpy as np
-
-from data_manager import DataManager
+from sklearn.linear_model import LogisticRegression
 from classifiers.classifier import Classifier
+from data_manager import DataManager
 
-def main ():
+
+def main():
+
     dm=DataManager()
-    
+    classifier=Classifier(dm, LogisticRegression)
+
+    classifier.train()
+
+    classifier._error()
+
+    classifier.show_stats()
+
 
 if __name__ == "__main__":
     main()
