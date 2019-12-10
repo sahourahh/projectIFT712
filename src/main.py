180cc45c2b6f9f49a9d465f7df0c32b2e4f0e743
sahar <tahir.sahar97@gmail.com>
2019-12-10 02:16:30 -0500

    class classifier to define train,predict and error methods

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
 from sklearn.linear_model import LogisticRegression
 from classifiers.classifier import Classifier
 from data_manager import DataManager
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 def main():
+    dm = DataManager()
 
-    dm=DataManager()
-    classifier=Classifier(dm, LogisticRegression)
+    lr = Classifier(dm, LogisticRegression(solver='liblinear', multi_class='auto'))
+    lr.train()
+    lr.show_stats()
 
-    classifier.train()
+    rf = Classifier(dm, RandomForestClassifier(n_estimators=10))
+    rf.train()
+    rf.show_stats()
 
-    classifier._error()
+    mlp = Classifier(dm, MLPClassifier(max_iter=20, warm_start=True, verbose=True, random_state=0))
+    mlp.train()
+    mlp.show_stats()
 
-    classifier.show_stats()
 
 
 if __name__ == "__main__":
