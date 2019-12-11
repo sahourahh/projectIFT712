1f8cc99ad6c513f6c55f6a3aa3f0e30d25d564fc
sahar <tahir.sahar97@gmail.com>
2019-12-11 01:36:20 -0500

    class classifier to define train,predict and error methods

--- src/classifiers/RandomForest.py
+++ src/classifiers/RandomForest.py
@@ -1,8 +1,9 @@
 from sklearn.ensemble import RandomForestClassifier
-class RandamForest(ParametricClassifier):
+from classifiers.parametric_classifier import ParametricClassifier
+import numpy as np
+class Randomforest(ParametricClassifier):
 
     def __init__(self, data_manager):
         super().__init__(data_manager)
         self.model = RandomForestClassifier(n_estimators=10)
-        self.param_grid = {"n_estimators": np.linspace(start = 200, stop = 2000, num = 10),
-                           "max_depth": np.linspace(10, 100, num = 10)}
+        self.param_grid = {"max_depth": np.linspace(10, 100, num = 10)}
