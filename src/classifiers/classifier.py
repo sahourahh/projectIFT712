2c33cd92d036ca135583163ab3099943a7670853
Aurélien Vauthier <vaua2803@usherbrooke.ca>
2019-12-11 12:42:21 -0500

    Added SVM classifier using RBF kernel

--- src/classifiers/classifier.py
+++ src/classifiers/classifier.py
@@ -1,3 +1,4 @@
+from sklearn.metrics import accuracy_score, log_loss
 
 class Classifier:
     def __init__(self, DataManager, classifier):
@@ -14,13 +15,17 @@ class Classifier:
         """
         self._clf.fit(self.X_train, self.t_train)
 
-    def prediction(self, X):
+    def prediction(self, X, predict_proba=False):
         """
         make predictions using new data instance
         :param X: data given
         :return: prediction of data given
         """
-        prediction = self._clf.predict(X)
+        if predict_proba:
+            prediction = self._clf.predict_proba(X)
+        else:
+            prediction = self._clf.predict(X)
+
         return prediction
 
     def _error(self ,X ,t):
@@ -33,9 +38,38 @@ class Classifier:
         error = self._clf.score(X ,t)
         return error
 
+    def loss(self, X, t):
+        """
+        Compute and return the cross entropy loss (or log loss) for the given X data and it's true targets t
+        :param X: the data to predict
+        :param t: the true data's targets
+        :return: the classifier's cross entropy loss on the given data
+        """
+        return log_loss(t, self.prediction(X, predict_proba=True))
+
+    def accuracy(self, X, t):
+        """
+        Compute and return the accuracy for the given X data and it's true targets t
+        :param X: the data to predict
+        :param t: the true data's targets
+        :return: the classifier's accuracy on the given data
+        """
+        return accuracy_score(t, self.prediction(X))
+
     def show_stats(self):
         """
-        show training error and test error
+        print training and test accuracy and loss
         """
-        print('Training error is {:.4f} %'.format(self._error(self.X_train ,self.t_train)*100 ))
-        print('Test error is {:.4f} %'.format(self._error(self.X_test ,self.t_test)*100 ))
+        # print classifier's class name
+        print("--- {} ---".format(self._clf.__class__.__name__))
+
+        # print training's accuracy and loss
+        print('Training results : accuracy is {:.4%}\tloss is {:.4f}'.format(
+            self.accuracy(self.X_train, self.t_train), self.loss(self.X_train, self.t_train) ))
+
+        # print test's accuracy and loss
+        print('Test results     : accuracy is {:.4%}\tloss is {:.4f}'.format(
+            self.accuracy(self.X_test, self.t_test), self.loss(self.X_test, self.t_test) ))
+
+        # print new line for readability
+        print()
