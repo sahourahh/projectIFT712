ddcb8baba5fd25b6c6be68a41f707fb7c344abf2
sahar <tahir.sahar97@gmail.com>
2019-12-11 01:45:01 -0500

    class classifier to define train,predict and error methods

--- src/classifiers/RandomForest.py
+++ src/classifiers/RandomForest.py
@@ -1,7 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
-from classifiers.parametric_classifier import ParametricClassifier
-import numpy as np
-class Randomforest(ParametricClassifier):
+class RandamForest(ParametricClassifier):
 
     def __init__(self, data_manager):
         super().__init__(data_manager)
