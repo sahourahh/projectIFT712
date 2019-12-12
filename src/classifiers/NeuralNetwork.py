6fe265e64d5cf4384eb408010b04b2ffae460c06
sahar <tahir.sahar97@gmail.com>
2019-12-12 17:25:29 -0500

    Added LogisticRegression

--- src/classifiers/NeuralNetwork.py
+++ src/classifiers/NeuralNetwork.py
@@ -4,12 +4,11 @@ import numpy as np
 from itertools import product
 class Neuralnetwork(ParametricClassifier):
 
-    def __init__(self, data_manager):
-        super().__init__(data_manager)
-        self.model = MLPClassifier(max_iter=10,warm_start=0)
+    def __init__(self, data_manager,useImageData):
+        super().__init__(data_manager,useImageData)
+        self.model = MLPClassifier(max_iter=100,warm_start=0)
 
-        self.param_grid = {'hidden_layer_sizes': list(product(range(100, 120), range(15, 20))),   # best= (104, 19)
-                            'learning_rate_init': np.linspace(start=0.01, stop=0.2, num=10),  # best=0.052222222222222225
+        self.param_grid = {'learning_rate_init': np.linspace(start=0.01, stop=0.5, num=20),  # best=0.01
                             'activation': ['tanh', 'relu', 'identity'],  # best=identity
                             'solver': ['sgd', 'adam', 'lbfgs'],  # best=adam
                            }
