dff547094539ba38fe980c2a5140c5ccd8816ca8
sahar <tahir.sahar97@gmail.com>
2019-12-12 03:25:37 -0500

    Multiple Layer Perceptron Classifier

--- src/classifiers/NeuralNetwork.py
+++ src/classifiers/NeuralNetwork.py
@@ -1,13 +1,14 @@
 from sklearn.neural_network import MLPClassifier
 from classifiers.parametric_classifier import ParametricClassifier
 import numpy as np
-class Neurolnetwork(ParametricClassifier):
+class Neuralnetwork(ParametricClassifier):
 
     def __init__(self, data_manager):
         super().__init__(data_manager)
         self.model = MLPClassifier(max_iter=10,warm_start=0)
 
-    self.param_grid = {'learning_rate_init': np.linspace(start=0.01, stop=0.5, num=10),  # best=0.11888888888888888
+    self.param_grid = {'hidden_layer_sizes': list(product(range(100, 120), range(15, 20))),   # best= (104, 19)
+                        'learning_rate_init': np.linspace(start=0.01, stop=0.2, num=10),  # best=0.052222222222222225
                        'activation': ['tanh', 'relu', 'identity'],  # best=identity
-                       'solver': ['sgd', 'adam', 'lbfgs'],  # adam
+                       'solver': ['sgd', 'adam', 'lbfgs'],  # best=adam
                        }
