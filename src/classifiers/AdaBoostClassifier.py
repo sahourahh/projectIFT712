5f11f913136d40e8ad88a053332dd7982a66b06e
Aurélien Vauthier <vaua2803@usherbrooke.ca>
2019-12-12 14:22:17 -0500

    Added useImageData option and changed tested parameters value to match useImageData=True

--- src/classifiers/AdaBoostClassifier.py
+++ src/classifiers/AdaBoostClassifier.py
@@ -5,11 +5,11 @@ from sklearn import ensemble, tree
 
 class AdaBoostClassifier(ParametricClassifier):
 
-    def __init__(self, data_manager):
-        super().__init__(data_manager)
+    def __init__(self, data_manager, useImageData):
+        super().__init__(data_manager, useImageData)
         self.model = ensemble.AdaBoostClassifier()
         self.param_grid = {
-            "base_estimator" : [tree.DecisionTreeClassifier(max_depth=n) for n in range(5, 6)], # best found : 5
-            "n_estimators" : range(50, 65), # best found : 57
-            "learning_rate" : np.geomspace(0.01, 1, num=15) # best found : 0.02131663116533842
+            "base_estimator" : [tree.DecisionTreeClassifier(max_depth=n) for n in range(13, 14)], # best found : 13
+            "n_estimators" : range(83, 89), # best found : 84
+            "learning_rate" : np.geomspace(0.01, 1, num=10) # best found : 0.027825
         }
