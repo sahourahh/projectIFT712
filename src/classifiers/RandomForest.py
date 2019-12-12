fb48b4510ccfa463ad2c9bcc548b4c7286f4959d
sahourahh <55034109+sahourahh@users.noreply.github.com>
2019-12-12 01:46:38 +0100

    Delete RandomForest.py

--- src/classifiers/RandomForest.py
+++
@@ -1,8 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-class RandamForest(ParametricClassifier):
-
-    def __init__(self, data_manager):
-        super().__init__(data_manager)
-        self.model = RandomForestClassifier(n_estimators=10)
-        self.param_grid = {"n_estimators": np.linspace(start = 200, stop = 2000, num = 10),
-                           "max_depth": np.linspace(10, 100, num = 10)}
