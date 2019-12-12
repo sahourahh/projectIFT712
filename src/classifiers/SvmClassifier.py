88379c22c5ef810695b31346fca74170ffc78b88
Aurélien Vauthier <vaua2803@usherbrooke.ca>
2019-12-12 01:13:53 -0500

    Renamed, file and class to match the new behavior

--- src/classifiers/SvmClassifier.py
+++ src/classifiers/SvmClassifier.py
@@ -3,7 +3,7 @@ from classifiers.parametric_classifier import ParametricClassifier
 import numpy as np
 from sklearn.svm import SVC
 
-class SvmRbf(ParametricClassifier):
+class SvmClassifier(ParametricClassifier):
 
     def __init__(self, data_manager):
         super().__init__(data_manager)
