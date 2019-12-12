4f091631aa5de14e27e3af2ebe3e910d9256faa4
sahar <tahir.sahar97@gmail.com>
2019-12-11 22:14:20 -0500

    LogisticRegression

--- src/classifiers/RandomForest.py
+++ src/classifiers/RandomForest.py
@@ -1,7 +1,9 @@
 from sklearn.ensemble import RandomForestClassifier
-class RandamForest(ParametricClassifier):
+from classifiers.parametric_classifier import ParametricClassifier
+import numpy as np
+class RandomForest(ParametricClassifier):
 
     def __init__(self, data_manager):
         super().__init__(data_manager)
-        self.model = RandomForestClassifier(n_estimators=10)
-        self.param_grid = {"max_depth": np.linspace(10, 100, num = 10)}
+        self.model = RandomForestClassifier(n_estimators=100)
+        self.param_grid = {"max_depth": np.linspace(10, 100, num = 10)} #best=20
