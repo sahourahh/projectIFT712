051b0d11a22daee22e416c5ca60d6d9a3ec3f8ed
Aurélien Vauthier <vaua2803@usherbrooke.ca>
2019-12-12 17:32:12 -0500

    Renamed class to match other classifier's name and add useImageData option

--- src/classifiers/LogisticReg.py
+++ src/classifiers/LogisticReg.py
@@ -1,9 +1,11 @@
 from sklearn.linear_model import LogisticRegression
 from classifiers.parametric_classifier import ParametricClassifier
-class Logisticregression(ParametricClassifier):
 
-    def __init__(self,data_manager):
+class LogisticRegressionClassifier(ParametricClassifier):
 
-        super().__init__(data_manager)
-        self.model=LogisticRegression(solver='liblinear',multi_class='auto')
-        self.param_grid={'penalty':['l1','l2']}
+    def __init__(self, data_manager, useImageData):
+        super().__init__(data_manager, useImageData)
+        self.model=LogisticRegression(solver='liblinear', multi_class='auto', max_iter=1000)
+        self.param_grid = {
+            'penalty' : ['l1','l2']
+        }
