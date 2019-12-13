6d1b2928efb63d1f02941d9d20c0dc2edaeb9c3f
Aurélien Vauthier <vaua2803@usherbrooke.ca>
2019-12-12 19:11:42 -0500

    LDA documentation update

--- src/classifiers/LinearDiscriminantAnalysisClassifier.py
+++ src/classifiers/LinearDiscriminantAnalysisClassifier.py
@@ -9,7 +9,7 @@ class LinearDiscriminantAnalysisClassifier(ParametricClassifier):
         super().__init__(data_manager, useImageData)
         self.model = LinearDiscriminantAnalysis()
         self.param_grid = {
-            # the 'svd' solver can not be use because of collinear data
+            # the 'eigen' solver can not be use with image data
             "solver": ['lsqr'],# 'eigen'],    # best found : 'lsqr'
             # furthermore, the shrinkage doesn't work with the 'svd' solver
             "shrinkage": np.geomspace(0.000000000001, 1).tolist() + [None, 'auto'] # best found : 4.71486636345739e-06
